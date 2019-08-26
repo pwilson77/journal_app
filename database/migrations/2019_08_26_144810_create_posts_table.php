c8e95e8e8c8cfa203f882a3ce4d5647fa3a7ce4f
pwilson77 <p.o.yirenkyi@gmail.com>
2019-08-26 15:59:03 +0000

    did migration for auth and posts

--- database/migrations/2019_08_26_144810_create_posts_table.php
+++ database/migrations/2019_08_26_144810_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->string('body');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
     }
